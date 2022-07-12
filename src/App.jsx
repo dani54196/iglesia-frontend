f0854876ce0697d740ba25e8d969890d1ce71f2f
Daniel Cardozo <dani54196@gmail.com>
2022-07-12 15:31:40 -0300

    index

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Vite Docs
+            hay que aprender petes !!
           </a>
         </p>
       </header>
