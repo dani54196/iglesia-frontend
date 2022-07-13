11255c8aa5c7b223be45a99d6e1c78a174b07fd7
Daniel Cardozo <dani54196@gmail.com>
2022-07-13 01:28:06 -0300

    react-router and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,22 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
+
+import Navigation from "./lib/Navigation";
+
+import Home from "./pages/home/Home"
+import Finance from "./pages/finance/Finance"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hay que aprender petes !!
-          </a>
-        </p>
-      </header>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/finance" element={<Finance/>} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
